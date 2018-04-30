7ca04d5fe07e32f4b5cc903a0355ad7007536194
Issei Fujimoto <otomijuf.004@gmail.com>
2018-04-30 22:03:20 +0900

    処理用のスクリプト追加

---
+++ main.py
@@ -0,0 +1,10 @@
+import string2image
+
+
+def main():
+    with open("./gomi.txt") as f:
+        for word in f.read():
+            string2image.string2image(value)
+
+if __name__ == '__main__':
+   main()
