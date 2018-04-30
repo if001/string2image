a0ced11fafffd2e225c399886a55a745d85e3c7c
Issei Fujimoto <otomijuf.004@gmail.com>
2018-04-30 21:17:11 +0900

    first

---
+++ conf.py
@@ -0,0 +1,8 @@
+import os
+class Conf():
+    font_size = 28
+    pict_size = 28
+    font_file = '/Users/issei/Library/Fonts/RictyDiminished-Regular.ttf'
+    run_dir_path = os.path.dirname(os.path.abspath(__file__))
+    save_dir = run_dir_path + "/image/"
+
---
+++ string2image.py
@@ -0,0 +1,51 @@
+from PIL import Image, ImageDraw, ImageFont
+from conf import Conf
+
+def save_image(inp, yomi):
+    font = ImageFont.truetype(Conf.font_file,Conf.font_size, encoding='unic')
+    image = Image.new('RGBA', (Conf.pict_size, Conf.pict_size))
+    draw = ImageDraw.Draw(image)
+
+    # 引数: (文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
+    draw.text((0, 0), inp, font = font, fill='#000000')
+    print("save " + Conf.save_dir + yomi + '.png')
+    image.save(Conf.save_dir + yomi + '.png', 'PNG')
+
+
+import os
+def is_exist(yomi):
+    return os.path.isfile(Conf.save_dir +  yomi + '.png')
+
+
+from pykakasi import kakasi
+
+def get_yomi(inp):
+    k = kakasi()
+    k.setMode('H', 'a')
+    k.setMode('K', 'a')
+    k.setMode('J', 'a')
+    k.setMode('E', 'a')
+
+    conv = k.getConverter()
+    return conv.do(inp)
+
+from kana2roma import Kana2Roma
+
+def string2image(inp):
+    yomi_str = get_yomi(inp)
+
+    if is_exist(yomi_str):
+        pass
+        # print(Conf.save_dir + yomi_str + '.png' + " is already exist")
+    else:
+        save_image(inp, yomi_str)
+    print(Conf.save_dir + yomi_str + '.png')
+
+def main():
+    inp = "我々は、今日も、歩く。"
+    for value in inp:
+        string2image(value)
+
+
+if __name__ == '__main__':
+    main()
