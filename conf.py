25c83f9142b93b581c965625478a67c739c49ae0
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-02 18:50:13 +0900

    gcpよう設定追加

--- conf.py
+++ conf.py
@@ -3,6 +3,7 @@ class Conf():
     font_size = 28
     pict_size = 28
     font_file = '/Users/issei/Library/Fonts/RictyDiminished-Regular.ttf'
+    font_file = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     save_dir = run_dir_path + "/image/"
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import string2image
 
 def main():
-    with open("../aozora_data/files/tmp.txt") as f:
+    with open("../aozora_data/files/files_all_rnp.txt") as f:
         for word in f.read():
             string2image.string2image(word)
 
