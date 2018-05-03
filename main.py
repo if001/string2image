730fe26f024ddb65be4bd2773af95d6dff9799e9
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-03 19:29:57 +0900

    改行、スペースは無視cc

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import string2image
 def main():
     with open("../aozora_data/files/files_all_rnp.txt") as f:
         for word in f.read():
-            string2image.string2image(word)
+            if (word != "") or (word != " ") or (word != "\n"):
+                string2image.string2image(word)
 
 if __name__ == '__main__':
    main()
