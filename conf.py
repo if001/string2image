db1d89a1a767397bc2ece716c84c7b57ee0fd015
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-07 14:42:47 +0900

    上下左右の反転処理追加、それに伴いスクリプトを修正

--- conf.py
+++ conf.py
@@ -1,10 +1,13 @@
 import os
+
+
 class Conf():
     font_size = 28
-    pict_size = 28
+    font_size_en = 32
+    pict_height = 28
+    pict_width = 28
     font_file = '/Users/issei/Library/Fonts/RictyDiminished-Regular.ttf'
     # font_file = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
     font_file = './RictyDiminished-Regular.ttf'
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     save_dir = run_dir_path + "/image/"
-
--- string2image.py
+++ string2image.py
@@ -1,47 +1,51 @@
-from PIL import Image, ImageDraw, ImageFont
-from conf import Conf
-img_pos = [(0,0),(0,3),(3,0),(3,3)]
-
-def save_image(inp, yomi):
-    font = ImageFont.truetype(Conf.font_file,Conf.font_size, encoding='unic')
-
-    # 1文字にあたり、少しだけ位置をずらした画像を4つ生成する
-    for i in range(len(img_pos)):
-        image = Image.new('RGB', (Conf.pict_size, Conf.pict_size), (255, 255, 255))
-        draw = ImageDraw.Draw(image)
-        draw.text(img_pos[i], inp, font = font, fill='#000000')
-        print("save " + Conf.save_dir + yomi + "_" + str(i) + '.png')
-        image.save(Conf.save_dir + yomi + "_" + str(i) + '.png', 'PNG')
-
-
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import unicodedata
 import os
+
+from conf import Conf
+
+
+def save_image(inp, yomi, font, prefix, processing=None, pos=(0, 0)):
+    image = Image.new(
+        'RGB', (Conf.pict_height, Conf.pict_width), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    draw.text(pos, inp, font=font, fill='#000000')
+    if processing is not None:
+        image = processing(image)
+    print("save " + Conf.save_dir + yomi + "_" + prefix + ".png")
+    image.save(Conf.save_dir + yomi + "_" + prefix + ".png", 'PNG')
+
+
 def is_exist(yomi):
-    return os.path.isfile(Conf.save_dir +  yomi + '.png')
+    return os.path.isfile(Conf.save_dir + yomi + '.png')
 
-
-from pykakasi import kakasi
-
-def get_yomi(inp):
-    k = kakasi()
-    k.setMode('H', 'a')
-    k.setMode('K', 'a')
-    k.setMode('J', 'a')
-    k.setMode('E', 'a')
-
-    conv = k.getConverter()
-    return conv.do(inp)
 
 def string2image(inp):
     yomi_str = inp
-
-    if is_exist(yomi_str + "_0"):
-        pass
-        # print(Conf.save_dir + yomi_str + '.png' + " is already exist")
+    if unicodedata.east_asian_width(inp) in 'FWA':  # 全角のとき
+        font = ImageFont.truetype(
+            Conf.font_file, Conf.font_size, encoding='unic')
     else:
-        save_image(inp, yomi_str)
+        font = ImageFont.truetype(
+            Conf.font_file, Conf.font_size_en, encoding='unic')
+
+    prefix = "flip"
+    if not is_exist(yomi_str + "_" + prefix):
+        save_image(inp, yomi_str, font, prefix, processing=ImageOps.flip)
+
+    prefix = "mirror"
+    if not is_exist(yomi_str + "_" + prefix):
+        save_image(inp, yomi_str, font, prefix, processing=ImageOps.mirror)
+
+    img_pos = [(0, 0), (0, 3), (3, 0), (3, 3)]
+    for i in range(len(img_pos)):
+        prefix = str(i)
+        if not is_exist(yomi_str + "_" + prefix):
+            save_image(inp, yomi_str, font, prefix, pos=img_pos[i])
+
 
 def main():
-    inp = "あ"
+    inp = "b"
     for value in inp:
         string2image(value)
 
