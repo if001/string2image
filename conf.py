e095e4c95d12c1cf286744d59c1715667643fe4f
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-07 19:27:21 +0900

    ヒラギノフォントで画像を作成する

--- conf.py
+++ conf.py
@@ -6,8 +6,7 @@ class Conf():
     font_size_en = 32
     pict_height = 28
     pict_width = 28
-    font_file = '/Users/issei/Library/Fonts/RictyDiminished-Regular.ttf'
-    # font_file = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
     font_file = './RictyDiminished-Regular.ttf'
+    font_file = './hiragino_maru_go_ProN_W4.ttc'
     run_dir_path = os.path.dirname(os.path.abspath(__file__))
     save_dir = run_dir_path + "/image/"
