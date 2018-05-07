42b27e4af4be67797f0d39b7328ff88e6ba8a5be
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-07 19:57:53 +0900

    アルゴリズムの修正

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import string2image
 
 
 def main():
+
     with open("../aozora_data/files/files_all_rnp.txt") as f:
-        for word in f.read():
-            if (word != "") or (word != " ") or (word != "\n"):
-                string2image.string2image2(word)
+        word_list = list(set(list(f.read())))
+
+    if '\n' in word_list:
+        word_list.remove('\n')
+    if ' ' in word_list:
+        word_list.remove(' ')
+    if '' in word_list:
+        word_list.remove('')
+
+    for word in word_list:
+        string2image.string2image2(word)
 
 
 if __name__ == '__main__':
