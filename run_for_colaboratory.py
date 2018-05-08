76f65916803850bc926e0ca9bad0a4325c2df221
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-08 13:37:41 +0900

    for folaboratory runner

---
+++ run_for_colaboratory.py
@@ -0,0 +1,21 @@
+import string2image
+
+
+def main():
+
+    with open("../aozora_data/files/files_all_rnp.txt") as f:
+        word_list = list(set(list(f.read())))
+
+    if '\n' in word_list:
+        word_list.remove('\n')
+    if ' ' in word_list:
+        word_list.remove(' ')
+    if '' in word_list:
+        word_list.remove('')
+
+    for word in word_list:
+        string2image.string2image(word, "./RictyDiminished-Regular.ttf")
+
+
+if __name__ == '__main__':
+    main()
