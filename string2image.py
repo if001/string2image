4461da03aa4281e5956728bf7420431f62e6353f
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-14 22:54:46 +0900

    importの不調和を修正

--- string2image.py
+++ string2image.py
@@ -126,9 +126,18 @@ def string2image(inp, font_file=None):
 
 
 def main():
-    inp = "b"
-    for value in inp:
-        string2image(value)
+    with open("../aozora_data/files/files_all_rnp.txt") as f:
+        word_list = list(set(list(f.read())))
+
+    if '\n' in word_list:
+        word_list.remove('\n')
+    if ' ' in word_list:
+        word_list.remove(' ')
+    if '' in word_list:
+        word_list.remove('')
+
+    for word in word_list:
+        string2image.string2image(word)
 
 
 if __name__ == '__main__':
